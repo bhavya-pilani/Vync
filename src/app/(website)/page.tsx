182ce9bb382839cc19468a40231bff8f4a01954c
bhavya-pilani <bhavyapilani10001@gmail.com>
2026-06-11 21:26:21 +0530

    Initial commit

---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Image
+        src="/opal-logo.svg"
+        alt="Vync"
+        width={100}
+        height={100}
+        style={{ width: "auto", height: "auto" }}
+      />
+    </div>
+  )
+}
